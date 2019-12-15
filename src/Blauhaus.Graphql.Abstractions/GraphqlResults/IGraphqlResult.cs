ff299434f65418a9457003cfbe847a6eac63c6fd
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-12-15 13:19:08 +0200

    abstractions

---
+++ src/Blauhaus.Graphql.Abstractions/GraphqlResults/IGraphqlResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.Graphql.Abstractions
+{
+    public interface IGraphqlResult<TPayload>
+    {
+        TPayload Payload { get; set; }
+        List<string> UserErrors { get; set; }
+    }
+}
