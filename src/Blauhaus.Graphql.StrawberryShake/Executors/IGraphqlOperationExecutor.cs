a10e3ec182fa422b975ebc358dde5c49c602c027
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-02-27 08:39:13 +0200

    no message

---
+++ src/Blauhaus.Graphql.StrawberryShake/Executors/IGraphqlOperationExecutor.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using StrawberryShake;
+
+namespace Blauhaus.Graphql.StrawberryShake.Executors
+{
+    public interface IGraphqlOperationExecutor<TOperationResult, in TOperation, TResult>
+        where TOperationResult : class, IOperationResult<TResult>
+        where TOperation  : IOperation<TResult>
+        where TResult : class
+    {
+        Task<TOperationResult> ExecuteAsync(TOperation operation, CancellationToken token);
+    }
+}
