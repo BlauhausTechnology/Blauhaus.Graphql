4395e42cf08861173f41467945bb6dcc4116e3b5
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-18 16:05:02 +0200

    no message

---
+++ src/Blauhaus.Graphql.HotChocolate/_Ioc/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Blauhaus.Graphql.HotChocolate.MutationHandlers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.Graphql.HotChocolate._Ioc
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAuthenticatedUserMutationHandler(this IServiceCollection services)
+        {
+            services.AddSingleton<IMutationServerHandler, AuthenticatedUserMutationServerHandler>();
+            return services;
+        }
+    }
+}
