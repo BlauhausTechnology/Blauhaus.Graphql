16f40ee3516418ed26529a579301ca4470ff7440
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-23 13:15:24 +0200

    no message

--- src/Blauhaus.Graphql.StrawberryShake/_Ioc/IocServiceExtensions.cs
+++ src/Blauhaus.Graphql.StrawberryShake/_Ioc/IocServiceExtensions.cs
@@ -8,16 +8,16 @@ namespace Blauhaus.Graphql.StrawberryShake._Ioc
     public static class IocServiceExtensions
     {
         public static IIocService AddClientEntityCommandHandler<
-                TModel, TModelDto, TGraphqlResponse, TCommandDto, TCommand, TMutationClient> 
+                TModel, TModelDto, TMutationResponse, TMutationClient, TCommandDto, TCommand> 
             (this IIocService services) 
             where TModel : class, IClientEntity 
-            where TGraphqlResponse : class
-            where TMutationClient : class, IMutationClient<TModelDto, TGraphqlResponse, TCommandDto, TCommand>
             where TModelDto : class
+            where TMutationResponse : class
+            where TMutationClient : class, IMutationClient<TModelDto, TMutationResponse, TCommandDto, TCommand>
         {
             services.RegisterImplementation<ICommandClientHandler<TModel, TCommand>, ClientEntityCommandHandler<TModel, TModelDto, TCommandDto, TCommand>>();
-            services.RegisterImplementation<IMutationClientHandler<TModelDto, TCommandDto>, MutationClientHandler<TModelDto, TGraphqlResponse, TCommandDto, TCommand>>();
-            services.RegisterImplementation<IMutationClient<TModelDto, TGraphqlResponse, TCommandDto, TCommand>, TMutationClient>();
+            services.RegisterImplementation<IMutationClientHandler<TModelDto, TCommandDto>, MutationClientHandler<TModelDto, TMutationResponse, TCommandDto, TCommand>>();
+            services.RegisterImplementation<IMutationClient<TModelDto, TMutationResponse, TCommandDto, TCommand>, TMutationClient>();
             services.RegisterImplementation<ICommandConverter<TCommandDto, TCommand>, TMutationClient>();
 
             return services;
