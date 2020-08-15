18df6311300b28dcf0f25b196957bbfca62e84f7
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-15 18:33:07 +0200

    no message

--- src/Blauhaus.Graphql.StrawberryShake/_Ioc/ServiceCollectionExtensions.cs
+++ src/Blauhaus.Graphql.StrawberryShake/_Ioc/ServiceCollectionExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net.Http;
 using Blauhaus.Auth.Abstractions._Ioc;
 using Blauhaus.Domain.Client.CommandHandlers;
+using Blauhaus.Domain.Client.Sync.CommandHandler;
 using Blauhaus.Domain.Common.CommandHandlers;
+using Blauhaus.Domain.Common.CommandHandlers.Sync;
 using Blauhaus.Domain.Common.Entities;
 using Blauhaus.Graphql.StrawberryShake.Config;
 using Blauhaus.Graphql.StrawberryShake.HttpClients;
@@ -20,6 +22,21 @@ namespace Blauhaus.Graphql.StrawberryShake._Ioc
             services.RegisterAccessToken();
             services.AddScoped<IGraphqlClientConfig, TConfig>();
             services.AddSingleton<IHttpClientFactory, HttpClientHolder>();
+
+            return services;
+        }
+
+        
+        public static IServiceCollection AddSyncClientQueryHandler<TModelDto, TMutationResponse, TSyncCommandDto, TSyncCommand, TQueryConverter>(this IServiceCollection services)
+            where TMutationResponse : class
+            where TQueryConverter : class, IQueryConverter<DtoSyncResult<TModelDto>, TMutationResponse, TSyncCommandDto, TSyncCommand>
+            where TModelDto : class
+            where TSyncCommand : SyncCommand
+            where TSyncCommandDto : notnull
+        { 
+            services.AddTransient<ICommandConverter<TSyncCommandDto, TSyncCommand>, TQueryConverter>();
+            services.AddTransient<IQueryConverter<DtoSyncResult<TModelDto>, TMutationResponse, TSyncCommandDto, TSyncCommand>, TQueryConverter>();
+            services.AddTransient<ICommandHandler<DtoSyncResult<TModelDto>, TSyncCommandDto>, ClientQueryHandler<DtoSyncResult<TModelDto>, TMutationResponse, TSyncCommandDto, TSyncCommand>>();
 
             return services;
         }
