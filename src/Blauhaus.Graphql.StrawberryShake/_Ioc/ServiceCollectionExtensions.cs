9440e9dace39190a274d59ced0b5e58320e2a9ab
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-15 17:39:25 +0200

    no message

--- src/Blauhaus.Graphql.StrawberryShake/_Ioc/ServiceCollectionExtensions.cs
+++ src/Blauhaus.Graphql.StrawberryShake/_Ioc/ServiceCollectionExtensions.cs
@@ -1,6 +1,10 @@
-﻿using Blauhaus.Domain.Client.CommandHandlers;
+﻿using System.Net.Http;
+using Blauhaus.Auth.Abstractions._Ioc;
+using Blauhaus.Domain.Client.CommandHandlers;
 using Blauhaus.Domain.Common.CommandHandlers;
 using Blauhaus.Domain.Common.Entities;
+using Blauhaus.Graphql.StrawberryShake.Config;
+using Blauhaus.Graphql.StrawberryShake.HttpClients;
 using Blauhaus.Graphql.StrawberryShake.QueryHandlers.Payload;
 using Blauhaus.Graphql.StrawberryShake.QueryHandlers.Void;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +13,17 @@ namespace Blauhaus.Graphql.StrawberryShake._Ioc
 {
     public static class ServiceCollectionExtensions
     {
+
+        public static IServiceCollection AddHttpClientHolder<TConfig>(this IServiceCollection services) where TConfig : class, IGraphqlClientConfig
+        {
+            
+            services.RegisterAccessToken();
+            services.AddScoped<IGraphqlClientConfig, TConfig>();
+            services.AddSingleton<IHttpClientFactory, HttpClientHolder>();
+
+            return services;
+        }
+
         public static IServiceCollection AddEntityClientQueryHandler<
             TModel, TModelDto, TMutationResponse, TCommandDto, TCommand, TMutationClient> 
                 (this IServiceCollection services) 
